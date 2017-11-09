01e2932a97b266f7b5ee7887cd0e7b09e4cf0998
何凯 <hekai@chalco-steering.com>
2017-11-09 16:56:01 +0800

    提交初始版本

---
+++ kgcx-opc/src/main/java/com/kgcx/KgcxOpcApplication.java
@@ -0,0 +1,13 @@
+package com.kgcx;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@MapperScan("com.kgcx.mapper.*")
+@SpringBootApplication
+public class KgcxOpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgcxOpcApplication.class, args);
+	}
+}
